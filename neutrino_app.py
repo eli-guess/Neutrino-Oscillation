c3412f26703ca64db828fd63aba3ccd598cad1b0
eli-guess <139928778+eli-guess@users.noreply.github.com>
2025-01-22 13:59:57 -0500

    Removed centering for mobile compatibility

--- neutrino_app.py
+++ neutrino_app.py
@@ -99,7 +99,6 @@ fig_distance = go.Figure()
 fig_distance.add_trace(go.Scatter(x=L_values, y=P_distance, mode='lines', name=f"Oscillation Probability"))
 fig_distance.update_layout(
     title = "Oscillation Probability vs Distance (Fixed Energy)",
-    title_x = 0.25,
     xaxis_title = "Distance (L) [km]",
     yaxis_title = "Oscillation Probability",
     template = "plotly_dark"
@@ -119,7 +118,6 @@ fig_energy = go.Figure()
 fig_energy.add_trace(go.Scatter(x=E_values, y=P_energy, mode='lines', name=f"Oscillation Probability"))
 fig_energy.update_layout(
     title = "Oscillation Probability vs Energy (Fixed Distance)",
-    title_x = 0.275,
     xaxis_title = "Energy (E) [GeV]",
     yaxis_title = "Oscillation Probability",
     template = "plotly_dark"
