be693abd4457864db986a4732d5e87c9ff9f6c6f
eli-guess <139928778+eli-guess@users.noreply.github.com>
2025-01-22 13:23:40 -0500

    Centered plot titles

--- neutrino_app.py
+++ neutrino_app.py
@@ -94,6 +94,7 @@ fig_distance = go.Figure()
 fig_distance.add_trace(go.Scatter(x=L_values, y=P_distance, mode='lines', name=f"Oscillation Probability"))
 fig_distance.update_layout(
     title="Oscillation Probability vs Distance (Fixed Energy)",
+    title_x = 0.25,
     xaxis_title="Distance (L) [km]",
     yaxis_title="Oscillation Probability",
     template="plotly_dark"
@@ -113,6 +114,7 @@ fig_energy = go.Figure()
 fig_energy.add_trace(go.Scatter(x=E_values, y=P_energy, mode='lines', name=f"Oscillation Probability"))
 fig_energy.update_layout(
     title="Oscillation Probability vs Energy (Fixed Distance)",
+    title_x = 0.275,
     xaxis_title="Energy (E) [GeV]",
     yaxis_title="Oscillation Probability",
     template="plotly_dark"
